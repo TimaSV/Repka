57ec15c5eed7e60fb11740a2e4e2e3a772741ea6
TimaSV <116054733+TimaSV@users.noreply.github.com>
2023-01-28 11:44:00 +0700

    Delete SearchEngine directory

--- SearchEngine/src/main/java/main/repository/interfaces/IndexRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package main.repository.interfaces;
-
-import main.model.Index;
-
-import java.util.List;
-
-public interface IndexRepositoryCustom {
-    void insertIndexList(String siteName, List<Index> indices);
-}
